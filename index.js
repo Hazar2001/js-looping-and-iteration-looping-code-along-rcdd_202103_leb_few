877ef2eb1f196fed0b05f3f3e351bf98b3f9013a
Hazar Saad ElDine <saadeldinehazar@gmail.com>
2021-04-13 09:02:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solutions in this file
+function writeCards("Ada", "Brendan", "Ali"], "birthday"){
+  const array=["Ada", "Brendan", "Ali"]
+  const name="birthday";
+  for (let i=0;i=<array.length;i++)
+  {
+    console.log("Thank you,"+ array[i] +",for the wonderful ${name} gift!");
+  }
+}
